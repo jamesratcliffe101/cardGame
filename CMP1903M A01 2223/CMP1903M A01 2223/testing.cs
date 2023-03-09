da181f63c9baa30e016b370d3a5ae49983a971eb
jamesratcliffe101 <35969385+jamesratcliffe101@users.noreply.github.com>
2023-03-09 00:50:36 +0000

    Add files via upload

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/testing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class testing
+    {
+        public testing()
+            {
+            // create a card pack
+            Pack cards = new Pack();
+
+            Pack.shuffleCardPack(2, Pack.cardpack);
+
+            List<Card> test = Pack.dealCard(52, Pack.cardpack);
+
+
+            for (int i = 0; i < test.Count; i++)
+            {
+               Console.WriteLine(test[i].Value.ToString() + "  " + test[i].Suit.ToString());
+            }
+
+
+
+
+            Console.ReadLine(); // to hold the program in place
+            }
+           
+    }
+}
