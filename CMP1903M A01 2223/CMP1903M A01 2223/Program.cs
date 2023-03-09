7bd8959003872d40d1d7fdaa9e4635c2f2968f44
jamesratcliffe101 <35969385+jamesratcliffe101@users.noreply.github.com>
2023-03-09 00:49:25 +0000

    Update Program.cs

--- CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,8 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
+
+            testing test = new testing();
 
         }
     }
